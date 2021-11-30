9113efa94b61eb904a2df1355c5bd7c015585ddc
Yahya Makarim <ymakarim@gmail.com>
2021-11-30 14:47:26 +0500

    cloudinary account changed

--- config/plugins.js
+++ config/plugins.js
@@ -1,30 +1,30 @@
 module.exports = () => {
   // ...
-    return {
-      // upload: {
-      //   provider: 'aws-s3',
-      //   providerOptions: {
-      //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      //     secretAccessKey: env('AWS_ACCESS_SECRET'),
-      //     region: env('AWS_REGION'),
-      //     params: {
-      //       Bucket: env('AWS_BUCKET'),
-      //     },
-      //   },
-      // },
-      upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: 'mehmoh41',
-          api_key: '458262958777453',
-          api_secret: '3PjJqZlMljZkTG-1JkIwEsijyGw',
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  return {
+    // upload: {
+    //   provider: 'aws-s3',
+    //   providerOptions: {
+    //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
+    //     secretAccessKey: env('AWS_ACCESS_SECRET'),
+    //     region: env('AWS_REGION'),
+    //     params: {
+    //       Bucket: env('AWS_BUCKET'),
+    //     },
+    //   },
+    // },
+    upload: {
+      provider: "iapadmin",
+      providerOptions: {
+        cloud_name: "iapadmin",
+        api_key: "965715939991726",
+        api_secret: "Wv3woIcgotS6i7Ma7WAecCl7du0",
       },
-    }
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  };
 
   // ...
 };

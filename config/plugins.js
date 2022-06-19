f711567fe428b88f645cfc39189f5baa6c5368df
mymakarim <ymakarim@gmail.com>
2022-06-19 13:07:04 +0300

    made for queue

--- config/plugins.js
+++ config/plugins.js
@@ -12,18 +12,18 @@ module.exports = () => {
     //     },
     //   },
     // },
-    upload: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: "iapadmin",
-        api_key: "965715939991726",
-        api_secret: "Wv3woIcgotS6i7Ma7WAecCl7du0",
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
+    // upload: {
+    //   provider: "cloudinary",
+    //   providerOptions: {
+    //     cloud_name: "iapadmin",
+    //     api_key: "965715939991726",
+    //     api_secret: "Wv3woIcgotS6i7Ma7WAecCl7du0",
+    //   },
+    //   actionOptions: {
+    //     upload: {},
+    //     delete: {},
+    //   },
+    // },
   };
 
   // ...
